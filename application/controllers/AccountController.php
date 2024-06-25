8c59ba1fb4a20d9cc62f0f898d29c5db208329ce
sai <162117966+saix0222@users.noreply.github.com>
2024-06-25 21:24:18 +0900

    p.280 registerアクションの追加

--- application/controllers/AccountController.php
+++ application/controllers/AccountController.php
@@ -8,4 +8,39 @@ class AccountController extends controller
             '_token' => $this->generateCsrfToken('account/signup'),
         ));
     }
+
+    public function registerAction()
+    {
+        if (!$this->request->isPost()){
+            $this->forward404();
+        }
+
+        $token = $this->request->getPost('_token');
+        if(!$this->checkCsrfToken('account/signup', $token)){
+            return $this->redirect('/account/signup');
+        }
+
+        $user_name = $this->request->getPost('user_name');
+        $password = $this->request->getPost('password');
+
+        $errors = array();
+
+        if(!strlen($user_name)){
+            $errors[] = 'ユーザIDを入力してください';
+        } else if (!preg_match('/^w{3,20}$/', $user_name)){
+            $errors[] = 'ユーザIDは半角英数およびアンダースコアを3～20文字以内で入力してください';
+        } else if (!$this->db_manager->get('User')->isUniquesUserName($user_name)){
+            $errors[] = 'ユーザIDは既に使用されています';
+        }
+
+        if(!strlen($password)){
+            $errors[] = 'パスワードを入力してください';
+        }else if(4 > strlen($password) || strlen($passwod) > 30){
+            $errors[] = 'パスワードは4～30文字以内で入力してください';
+        }
+
+        if(count($errors) === 0){
+            $this->db_manager->get('User')->insert($user_name, $password);
+        }
+    }
 }
