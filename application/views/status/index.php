ca6d56f5b125afb0cc38f26260237b5a0429cc17
sai <162117966+saix0222@users.noreply.github.com>
2024-06-26 16:28:26 +0900

    p.289-290 エラー表示共通化

--- application/views/status/index.php
+++ application/views/status/index.php
@@ -5,7 +5,9 @@
 <form action = "<?php echo $base_url; ?>/status/post" method="post">
     <input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>" />
 
-    <?php //エラー処理 ?>
+    <?php if (isset($errors) && count($errors) > 0): ?>
+    <?php echo $this->render('errors', array('errors' => $errors)); ?>
+    <?php endif; ?>
 
     <textarea name="body" rows="2" cols="60"><?php echo $this->escape($body); ?></textarea>
 
