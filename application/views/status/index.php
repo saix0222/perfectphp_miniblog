18e74dde3ed8244051b35cad17eb00c9e853a752
sai <162117966+saix0222@users.noreply.github.com>
2024-06-26 16:53:12 +0900

    p.290-291 投稿表示、ユーザ情報入力の共通化

--- application/views/status/index.php
+++ application/views/status/index.php
@@ -2,11 +2,11 @@
 
 <h2>ホーム</h2>
 
-<form action = "<?php echo $base_url; ?>/status/post" method="post">
+<form action="<?php echo $base_url; ?>/status/post" method="post">
     <input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>" />
 
-    <?php if (isset($errors) && count($errors) > 0): ?>
-    <?php echo $this->render('errors', array('errors' => $errors)); ?>
+    <?php if (isset($errors) && count($errors) > 0) : ?>
+        <?php echo $this->render('errors', array('errors' => $errors)); ?>
     <?php endif; ?>
 
     <textarea name="body" rows="2" cols="60"><?php echo $this->escape($body); ?></textarea>
@@ -16,16 +16,8 @@
     </p>
 
     <div id="statuses">
-        <?php foreach ($statuses as $status): ?>
-        <div class="status">
-            <div class="status_content">
-                <?php echo $this->escape($status['user_name']); ?>
-                <?php echo $this->escape($status['body']); ?>
-            </div>
-            <div>
-                <?php echo $this->escape($status['created_at']); ?>
-            </div>
-        </div>
+        <?php foreach ($statuses as $status) : ?>
+        <?php echo $this->render('status/status', array('status' => $status)); ?>
         <?php endforeach; ?>
     </div>
 </form>
